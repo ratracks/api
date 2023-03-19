af460aec3f148a40d61fd38b0fb46af3ec60ca82
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-03-19 14:54:00 -0300

    Creating the project

---
+++ src/main/java/com/ratracks/RatracksApiApplication.java
@@ -0,0 +1,13 @@
+package com.ratracks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatracksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatracksApiApplication.class, args);
+	}
+
+}
