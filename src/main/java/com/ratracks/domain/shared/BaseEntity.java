6705d9f9f5ffe0f194680fefb0dcb75e95c588ac
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-03-31 20:13:59 -0300

    refactor(entities/BaseEntity): adding lombok @Data at the base entity

--- src/main/java/com/ratracks/domain/shared/BaseEntity.java
+++ src/main/java/com/ratracks/domain/shared/BaseEntity.java
@@ -3,13 +3,9 @@ package com.ratracks.domain.shared;
 import java.util.Date;
 import java.util.UUID;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
-@Getter
-@Setter
-@ToString
+@Data
 public class BaseEntity {
 
     public BaseEntity() {
