e117e465e8ba711118ed406c0ed9ab94b61e5b1c
AlexanderPoliser <alexanderpoliser@gmail.com>
2023-04-02 14:32:22 -0300

    refactor(exceptions): changing the visibility of the GenericException constructor to "protected" to fix code smell

--- src/main/java/com/ratracks/exceptions/GenericException.java
+++ src/main/java/com/ratracks/exceptions/GenericException.java
@@ -2,7 +2,7 @@ package com.ratracks.exceptions;
 
 public abstract class GenericException extends RuntimeException {
     
-    public GenericException(String message, Throwable cause) {
+    protected GenericException(String message, Throwable cause) {
         super(message, cause);
     }
 }
