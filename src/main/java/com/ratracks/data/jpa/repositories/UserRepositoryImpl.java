cf17d842d9137ce167fe1666b10b9ec3e5bee23c
AlexanderPoliser <alexanderpoliser@gmail.com>
2023-04-02 14:12:55 -0300

    feature(data/jpa/repositories): adding UserRepositoryImpl and JpaUserRepository for data persistence

---
+++ src/main/java/com/ratracks/data/jpa/repositories/UserRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.ratracks.data.jpa.repositories;
+
+import org.springframework.stereotype.Repository;
+
+import com.ratracks.data.schemas.UserSchema;
+import com.ratracks.domain.contracts.repositories.UserRepository;
+import com.ratracks.domain.entities.User;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+    private JpaUserRepository repository;
+
+    public UserRepositoryImpl(JpaUserRepository repository){
+        this.repository = repository;
+    }
+
+    @Override
+    public void create(User user) {
+        try {
+            UserSchema userSchema = new UserSchema(user.getName());
+            repository.save(userSchema);
+        } catch (Exception e) {
+            throw new RuntimeException("Error creating user", e);
+        }
+    }
+}
