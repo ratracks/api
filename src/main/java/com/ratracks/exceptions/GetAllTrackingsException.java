8eac00865188ac2e489a89d8a8696bebc9ab8247
AlexanderPoliser <alexanderpoliser@gmail.com>
2023-05-19 17:08:31 -0300

    feat(exceptions): adding custom exceptions to each method

---
+++ src/main/java/com/ratracks/exceptions/GetAllTrackingsException.java
@@ -0,0 +1,7 @@
+package com.ratracks.exceptions;
+
+public class GetAllTrackingsException extends GenericException{
+    public GetAllTrackingsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
