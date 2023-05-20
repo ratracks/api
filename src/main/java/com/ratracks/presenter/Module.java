adceaab36cb92751672ceef9a39572674b23343b
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-05-20 12:47:55 -0300

    RAT-45 feature(module): adding get trackings usecase bean

--- src/main/java/com/ratracks/presenter/Module.java
+++ src/main/java/com/ratracks/presenter/Module.java
@@ -3,8 +3,10 @@ package com.ratracks.presenter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.ratracks.domain.contracts.repositories.TrackingRepository;
 import com.ratracks.domain.contracts.repositories.UserRepository;
 import com.ratracks.domain.usecases.CreateAnonymousUserUsecase;
+import com.ratracks.domain.usecases.GetTrackingsUseCase;
 
 @Configuration
 public class Module {
@@ -12,6 +14,11 @@ public class Module {
     @Bean
     public CreateAnonymousUserUsecase createAnonymousUserUsecase(UserRepository repository) {
         return new CreateAnonymousUserUsecase(repository);
+    }
+
+    @Bean
+    public GetTrackingsUseCase getTrackingsUseCase(TrackingRepository repository) {
+        return new GetTrackingsUseCase(repository);
     }
 
 }
